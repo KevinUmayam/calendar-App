6ee5aef3211c2dae51df67f3439bbd593090d3e7
Kevin U <owner@Kevins-MacBook-Pro.local>
2022-07-18 22:25:10 -0700

    managed to get things to save to local storage.

--- Calendar assets/calandar.js
+++ Calendar assets/calandar.js
@@ -1,12 +1,15 @@
 var DisplaytimeEl = $("#todaysDate");
 const textareaC = document.querySelectorAll(".colorT");
-const saveBtn = document.querySelector(".btn");
+const saveBtn = $(".btn");
 let savedNotes = {};
 // time
 let today = new Date();
 let currentHour = today.getHours();
 console.log(currentHour);
 console.log(textareaC);
+let timeID;
+let textInput;
+
 function displayTime() {
   var todaysDate = moment().format("dddd, MMMM Do [:] hh:mm A[||]GGGG");
   DisplaytimeEl.text(todaysDate);
@@ -14,29 +17,49 @@ function displayTime() {
 
 setInterval(displayTime, 1000);
 
-let TextA = document.getElementsByClassName("colorT");
-let arrayHtml = [].slice.call(TextA);
+// let TextA = document.getElementsByClassName("colorT");
+// let arrayHtml = [].slice.call(TextA);
 
-console.log(arrayHtml);
-arrayHtml.forEach(function (textArea) {
-  //   get integer from id with parseInt https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseInt
-  let boxhour = parseInt($(textArea).attr("id"));
+// console.log(arrayHtml);
+// arrayHtml.forEach(function (textArea) {
+//   //   get integer from id with parseInt https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseInt
+//   let boxhour = parseInt($(textArea).attr("id"));
 
-  console.log(boxhour, currentHour);
-  console.log(typeof currentHour);
+//   console.log(boxhour, currentHour);
+//   console.log(typeof currentHour);
 
-  if (boxhour < currentHour) {
-    $("textarea").removeClass("future present").addClass("past");
-    console.log("hello");
-  } else if (boxhour === currentHour) {
-    $("textarea").removeClass("past future").addClass("present");
-    console.log("hi");
-  } else {
-    $("textarea").removeClass("past present").addClass("future");
-    console.log("happy");
+//   if (boxhour < currentHour) {
+//     $("textarea").removeClass("future present").addClass("past");
+//     console.log("hello");
+//   } else if (boxhour === currentHour) {
+//     $("textarea").removeClass("past future").addClass("present");
+//     console.log("hi");
+//   } else {
+//     $("textarea").removeClass("past present").addClass("future");
+//     console.log("happy");
+//   }
+// });
+function saveTLS() {
+  window.localStorage.setItem(timeID, textInput);
+}
+
+function updateTextA() {
+  for (let hour = 9; hour <= 20; hour++) {
+    var textArea = $("#hour-" + hour);
+    if (hour < currentHour) {
+      textArea.removeClass("future present").addClass("past");
+      console.log("hello");
+    } else if (hour === currentHour) {
+      textArea.removeClass("past future").addClass("present");
+      console.log("hi");
+    } else {
+      textArea.removeClass("past present").addClass("future");
+      console.log("happy");
+    }
   }
-});
-
+  //   window.localStorage.getItem(hour);
+}
+updateTextA();
 // function past() {
 //   //   const changeB = document.getElementsByTagName("textarea");
 //   //   changeB.removeAttribute("class", "future");
@@ -44,10 +67,15 @@ arrayHtml.forEach(function (textArea) {
 //   //   document.getElementsByTagName("textarea").removeAttribute("class", "future");
 // }
 
-saveBtn.addEventListener("click", (e) => {
+saveBtn.click(function (e) {
   e.preventDefault();
-  console.log(e.target);
-  arrayHtml.forEach(function (currentNotes) {});
+
+  textInput = $(this).siblings(".colorT").val();
+
+  timeID = $(this).siblings(".colorT").attr("id");
+
+  //   arrayHtml.forEach(function (currentNotes) {});
+  saveTLS();
 });
 
 // $(textareaC).removeClass("future");
