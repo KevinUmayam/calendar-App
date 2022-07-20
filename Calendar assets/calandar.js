7eab9555c21b02a859dafa4c323d0ccfeb23050b
Kevin U <owner@Kevins-MacBook-Pro.local>
2022-07-19 18:40:29 -0700

    was able to get the stored information to display in the textareas.

--- Calendar assets/calandar.js
+++ Calendar assets/calandar.js
@@ -1,5 +1,5 @@
 var DisplaytimeEl = $("#todaysDate");
-const textareaC = document.querySelectorAll(".colorT");
+const textareaC = $(".colorT");
 const saveBtn = $(".btn");
 let savedNotes = {};
 // time
@@ -11,34 +11,12 @@ let timeID;
 let textInput;
 
 function displayTime() {
-  var todaysDate = moment().format("dddd, MMMM Do [:] hh:mm A[||]GGGG");
+  var todaysDate = moment().format("dddd, MMMM Do [||] hh:mm A[||]GGGG");
   DisplaytimeEl.text(todaysDate);
 }
 
 setInterval(displayTime, 1000);
 
-// let TextA = document.getElementsByClassName("colorT");
-// let arrayHtml = [].slice.call(TextA);
-
-// console.log(arrayHtml);
-// arrayHtml.forEach(function (textArea) {
-//   //   get integer from id with parseInt https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseInt
-//   let boxhour = parseInt($(textArea).attr("id"));
-
-//   console.log(boxhour, currentHour);
-//   console.log(typeof currentHour);
-
-//   if (boxhour < currentHour) {
-//     $("textarea").removeClass("future present").addClass("past");
-//     console.log("hello");
-//   } else if (boxhour === currentHour) {
-//     $("textarea").removeClass("past future").addClass("present");
-//     console.log("hi");
-//   } else {
-//     $("textarea").removeClass("past present").addClass("future");
-//     console.log("happy");
-//   }
-// });
 function saveTLS() {
   window.localStorage.setItem(timeID, textInput);
 }
@@ -56,31 +34,18 @@ function updateTextA() {
       textArea.removeClass("past present").addClass("future");
       console.log("happy");
     }
+    textArea.val(window.localStorage.getItem("hour-" + hour));
   }
-  //   window.localStorage.getItem(hour);
 }
 updateTextA();
-// function past() {
-//   //   const changeB = document.getElementsByTagName("textarea");
-//   //   changeB.removeAttribute("class", "future");
-//   //   changeB.setAttribute("class", "past");
-//   //   document.getElementsByTagName("textarea").removeAttribute("class", "future");
-// }
 
 saveBtn.click(function (e) {
   e.preventDefault();
 
   textInput = $(this).siblings(".colorT").val();
-
+  console.log(textInput);
   timeID = $(this).siblings(".colorT").attr("id");
-
-  //   arrayHtml.forEach(function (currentNotes) {});
+  console.log(timeID);
+  //   arrayHtml.forEach(function (currentNotes) {});`
   saveTLS();
 });
-
-// $(textareaC).removeClass("future");
-// $(textareaC).addClass("past");
-// $(textareaC).removeClass("past");
-// $(textareaC).addClass("present");
-// $(textareaC).removeClass("present");
-// $(textareaC).addClass("future");
